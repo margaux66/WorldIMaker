982fbb863c39dcfea3cf0e2c7062a00437873485
victorinemrl <victorine.maurel@hotmail.fr>
2019-12-29 21:13:25 +0100

    change bg color

--- src/main.cpp
+++ src/main.cpp
@@ -143,7 +143,7 @@ int main(int argc, char const *argv[]){
          *********************************/
 
         //Clear the window
-        glClearColor(1,0,0,1);
+        glClearColor(0,0,0,1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         scene.applyDirectionalLight(camera);
